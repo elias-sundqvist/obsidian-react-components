81d5c6ffc3c37fca3b5897791275fde6f54f25f8
lucasew <lucas59356@gmail.com>
2021-05-06 17:15:26 -0300

    format via vscode
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- main.tsx
+++ main.tsx
@@ -1,205 +1,244 @@
-import { App, MarkdownPostProcessorContext, normalizePath, Notice, Plugin, PluginSettingTab, Setting, TAbstractFile, TFile, TFolder, Vault } from 'obsidian';
-import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
-import Babel from '@babel/standalone';
-import ReactPreset from '@babel/preset-react';
+import {
+  MarkdownPostProcessorContext,
+  normalizePath,
+  Notice,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+  TAbstractFile,
+  TFile,
+  TFolder,
+  Vault,
+} from "obsidian";
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import Babel from "@babel/standalone";
+import ReactPreset from "@babel/preset-react";
 
 const DEFAULT_SETTINGS: ReactBlocksSettings = {
-	template_folder: ""
+  template_folder: "",
 };
 
 interface ReactBlocksSettings {
-	template_folder: string;
+  template_folder: string;
 }
 
 export default class ReactBlocksPlugin extends Plugin {
-	settings: ReactBlocksSettings;
-	codeBlocks: Map<string, string>
-	components: Record<string, (any) => JSX.Element> = {}
+  settings: ReactBlocksSettings;
+  codeBlocks: Map<string, string>;
+  components: Record<string, (any) => JSX.Element> = {};
 
-	getScope() {
-		const isPreviewMode = () => this.app.workspace.activeLeaf.view.getState() === "preview";
-		return {
-			...this.components,
-			React,
-			ReactDOM,
-			useState,
-			useEffect,
-			isPreviewMode
-		}
-	}
+  getScope() {
+    const isPreviewMode = () =>
+      this.app.workspace.activeLeaf.view.getState() === "preview";
+    return {
+      ...this.components,
+      React,
+      ReactDOM,
+      useState,
+      useEffect,
+      isPreviewMode,
+    };
+  }
 
-	getScopeExpression(scope = this.getScope()) {
-		return Object.keys(scope).sort().map(k=>`let ${k}=scope.${k};`).join("\n")+"\n";
-	}
+  getScopeExpression(scope = this.getScope()) {
+    return (
+      Object.keys(scope)
+        .sort()
+        .map((k) => `let ${k}=scope.${k};`)
+        .join("\n") + "\n"
+    );
+  }
 
-	transpileCode(content: string) {
-		return Babel.transform(content, {presets: [ReactPreset]}).code
-	}
+  transpileCode(content: string) {
+    return Babel.transform(content, { presets: [ReactPreset] }).code;
+  }
 
-	// evaluated code inherits the scope of the current function
-	evalAdapter(code: string, scope = this.getScope()) {
-		return eval(code)
-	}
+  // evaluated code inherits the scope of the current function
+  evalAdapter(code: string, scope = this.getScope()) {
+    return eval(code);
+  }
 
-	async registerComponent(file: TFile){
-		const content = await this.app.vault.read(file)
-		const code = `props=>{
+  async registerComponent(file: TFile) {
+    const content = await this.app.vault.read(file);
+    const code = `props=>{
 			${this.getScopeExpression()}
 			${content}
-		}`
-		if(!(this.codeBlocks.has(file.basename) && this.codeBlocks[file.basename]==code)) {
-			this.codeBlocks[file.basename] = code
-			this.app.workspace.trigger('react-components:component-updated')
-		}
-		try {
-			this.components[file.basename] = this.evalAdapter(this.transpileCode(this.codeBlocks[file.basename]));
-		} catch (e) {
-			console.error(e)
-			console.log(`failed file: ${file.path}`)
-		}
-	}
+		}`;
+    if (
+      !(
+        this.codeBlocks.has(file.basename) &&
+        this.codeBlocks[file.basename] == code
+      )
+    ) {
+      this.codeBlocks[file.basename] = code;
+      this.app.workspace.trigger("react-components:component-updated");
+    }
+    try {
+      this.components[file.basename] = this.evalAdapter(
+        this.transpileCode(this.codeBlocks[file.basename])
+      );
+    } catch (e) {
+      console.error(e);
+      console.log(`failed file: ${file.path}`);
+    }
+  }
 
-	loadComponents() {
-		this.components = {}
-		if(this.settings.template_folder.trim()=="") {
-			new Notice("Cannot Load react components unless directory is set")
-		} else {
-			try {
-				let files = this.getTFilesFromFolder(this.settings.template_folder);
-				for (let file of files) {
-					this.registerComponent(file);
-				}
-			} catch(e){
-				new Notice("React Component Folder Not Found!")
-			}
-		}
-	}
+  loadComponents() {
+    this.components = {};
+    if (this.settings.template_folder.trim() == "") {
+      new Notice("Cannot Load react components unless directory is set");
+    } else {
+      try {
+        let files = this.getTFilesFromFolder(this.settings.template_folder);
+        for (let file of files) {
+          this.registerComponent(file);
+        }
+      } catch (e) {
+        new Notice("React Component Folder Not Found!");
+      }
+    }
+  }
 
-	async attachComponent(source: string, el:HTMLElement, ctx:MarkdownPostProcessorContext) {
-		let tryRender = () => {
-			try {
-				const expr = `
+  async attachComponent(
+    source: string,
+    el: HTMLElement,
+    ctx: MarkdownPostProcessorContext
+  ) {
+    let tryRender = () => {
+      try {
+        const expr = `
 				${this.getScopeExpression()}
 				${source}
-				`
-				let evaluated = this.evalAdapter(this.transpileCode(expr))
-				ReactDOM.render(evaluated, el)
-			} catch(e) {
-				console.error(e)
-				console.log(`failed file: ${ctx.sourcePath}`)
-				ReactDOM.render(<div style={{color: "red"}}>{e.toString()}</div>, el)
-			}
-		}
-		tryRender()
-		let evRef = this.app.workspace.on('react-components:component-updated', ()=>{
-			if(el){
-				tryRender()
-			} else {
-				this.app.workspace.offref(evRef)
-			}
-		})
-	}
+				`;
+        let evaluated = this.evalAdapter(this.transpileCode(expr));
+        ReactDOM.render(evaluated, el);
+      } catch (e) {
+        console.error(e);
+        console.log(`failed file: ${ctx.sourcePath}`);
+        ReactDOM.render(<div style={{ color: "red" }}>{e.toString()}</div>, el);
+      }
+    };
+    tryRender();
+    let evRef = this.app.workspace.on(
+      "react-components:component-updated",
+      () => {
+        if (el) {
+          tryRender();
+        } else {
+          this.app.workspace.offref(evRef);
+        }
+      }
+    );
+  }
 
-	async onload() {
-		await this.loadSettings();
-		this.loadComponents()
-		this.codeBlocks = new Map();
-		let registerIfCodeBlockFile = (file) => {
-			if(this.settings.template_folder != "" && file.parent.path.startsWith(this.settings.template_folder)) {
-				this.registerComponent(file)
-			}
-		}
-		this.app.metadataCache.on('changed', registerIfCodeBlockFile)
-		this.app.metadataCache.on('resolve', registerIfCodeBlockFile)
-		this.app.workspace.on('layout-ready', ()=>this.loadComponents())
-		this.registerMarkdownCodeBlockProcessor("jsx-", this.attachComponent.bind(this))
-		this.registerMarkdownPostProcessor(async (el, ctx) => {
-			let codeblocks = el.querySelectorAll("code");
-			let toReplace = []
-			for (let index = 0; index < codeblocks.length; index++) {
-				let codeblock = codeblocks.item(index);
+  async onload() {
+    await this.loadSettings();
+    this.loadComponents();
+    this.codeBlocks = new Map();
+    let registerIfCodeBlockFile = (file) => {
+      if (
+        this.settings.template_folder != "" &&
+        file.parent.path.startsWith(this.settings.template_folder)
+      ) {
+        this.registerComponent(file);
+      }
+    };
+    this.app.metadataCache.on("changed", registerIfCodeBlockFile);
+    this.app.metadataCache.on("resolve", registerIfCodeBlockFile);
+    this.app.workspace.on("layout-ready", () => this.loadComponents());
+    this.registerMarkdownCodeBlockProcessor(
+      "jsx-",
+      this.attachComponent.bind(this)
+    );
+    this.registerMarkdownPostProcessor(async (el, ctx) => {
+      let codeblocks = el.querySelectorAll("code");
+      let toReplace = [];
+      for (let index = 0; index < codeblocks.length; index++) {
+        let codeblock = codeblocks.item(index);
 
-				let text = codeblock.innerText.trim();
-				if (!text.startsWith("jsx-")) continue;
+        let text = codeblock.innerText.trim();
+        if (!text.startsWith("jsx-")) continue;
 
-				let source = text.substring("jsx-".length).trim();
-				toReplace.push({codeblock, source})
-			}
-			toReplace.forEach(({codeblock, source})=>{
-				let container = document.createElement("span");
-				codeblock.replaceWith(container)
-				this.attachComponent(source, container, ctx)
-			})
-		});
-		this.addSettingTab(new ReactBlocksSettingTab(this));
-	}
+        let source = text.substring("jsx-".length).trim();
+        toReplace.push({ codeblock, source });
+      }
+      toReplace.forEach(({ codeblock, source }) => {
+        let container = document.createElement("span");
+        codeblock.replaceWith(container);
+        this.attachComponent(source, container, ctx);
+      });
+    });
+    this.addSettingTab(new ReactBlocksSettingTab(this));
+  }
 
-	onunload() {
-		console.log('unloading plugin');
-	}
+  onunload() {
+    console.log("unloading plugin");
+  }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-	getTFilesFromFolder(folder_str: string): Array<TFile> {
-		folder_str = normalizePath(folder_str);
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 
-		let folder = this.app.vault.getAbstractFileByPath(folder_str);
-		if (!folder) {
-			throw new Error(`${folder_str} folder doesn't exist`);
-		}
-		if (!(folder instanceof TFolder)) {
-			throw new Error(`${folder_str} is a file, not a folder`);
-		}
+  getTFilesFromFolder(folder_str: string): Array<TFile> {
+    folder_str = normalizePath(folder_str);
 
-		let files: Array<TFile> = [];
-		Vault.recurseChildren(folder, (file: TAbstractFile) => {
-			if (file instanceof TFile) {
-				files.push(file);
-			}
-		});
+    let folder = this.app.vault.getAbstractFileByPath(folder_str);
+    if (!folder) {
+      throw new Error(`${folder_str} folder doesn't exist`);
+    }
+    if (!(folder instanceof TFolder)) {
+      throw new Error(`${folder_str} is a file, not a folder`);
+    }
 
-		files.sort((a, b) => {
-			return a.basename.localeCompare(b.basename);
-		});
+    let files: Array<TFile> = [];
+    Vault.recurseChildren(folder, (file: TAbstractFile) => {
+      if (file instanceof TFile) {
+        files.push(file);
+      }
+    });
 
-		return files;
-	}
+    files.sort((a, b) => {
+      return a.basename.localeCompare(b.basename);
+    });
+
+    return files;
+  }
 }
 
-
-
 class ReactBlocksSettingTab extends PluginSettingTab {
-	plugin: ReactBlocksPlugin;
+  plugin: ReactBlocksPlugin;
 
-	constructor(plugin: ReactBlocksPlugin) {
-		super(plugin.app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(plugin: ReactBlocksPlugin) {
+    super(plugin.app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		let {containerEl} = this;
+  display(): void {
+    let { containerEl } = this;
 
-		containerEl.empty();
+    containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Obsidian React Components Settings'});
+    containerEl.createEl("h2", { text: "Obsidian React Components Settings" });
 
-		new Setting(containerEl)
-			.setName("Components folder location")
-			.setDesc("Files in this folder will be available as components/functions.")
-			.addText(text => {
-				text.setPlaceholder("Example: folder 1/folder 2")
-					.setValue(this.plugin.settings.template_folder)
-					.onChange((new_folder) => {
-						this.plugin.settings.template_folder = new_folder;
-						this.plugin.loadComponents()
-						this.plugin.saveSettings();
-					})
-			});
-	}
+    new Setting(containerEl)
+      .setName("Components folder location")
+      .setDesc(
+        "Files in this folder will be available as components/functions."
+      )
+      .addText((text) => {
+        text
+          .setPlaceholder("Example: folder 1/folder 2")
+          .setValue(this.plugin.settings.template_folder)
+          .onChange((new_folder) => {
+            this.plugin.settings.template_folder = new_folder;
+            this.plugin.loadComponents();
+            this.plugin.saveSettings();
+          });
+      });
+  }
 }
